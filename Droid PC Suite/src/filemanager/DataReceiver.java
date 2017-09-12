a6690ccfd2dbd5715bd9e54b3a067ce652d41ad4
Karanvir Singh <karanvir.singh61@yahoo.in>
2017-09-13 01:25:19 +0530

    Update DataReceiver.java

--- Droid PC Suite/src/filemanager/DataReceiver.java
+++ Droid PC Suite/src/filemanager/DataReceiver.java
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * filemanager/DataReceiver.java: DataReceiver class for Droid PC Suite
  *****************************************************************************
- * Copyright (C) 2016 Karanvir Singh
+ * Copyright (C) 2017 Karanvir Singh
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
